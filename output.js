c77d895b6933f97820c7dd5aef6fd978f733638e
B0R0V <57464725+B0R0V@users.noreply.github.com>
2021-03-15 18:55:25 +0300

    up

--- output.js
+++ output.js
@@ -2,14 +2,18 @@ window.addEventListener('load', function() {
   _log=console.log;
   _objectView = {
     GetChainProto: function(arg) {
-      return s.call(arg);function s(e) {
-        let t;t = "string" === e ? new String("") : "number" === e ? new Number(0) : "bigint" === e ? Object(BigInt(0)) : "boolean" === e ? new Boolean(!1) : this;const n = [];  try {for (let i = t; i; i = Object.getPrototypeOf(i)) {if (("array" === e || "typedarray" === e) && i === t && i.length > 9999) continue;   const s = {     items: [],     __proto__: null   };   try {     "object" == typeof i && Object.prototype.hasOwnProperty.call(i, "constructor") && i.constructor && i.constructor.name && (s.title = i.constructor.name)   } catch (e) {}   n[n.length] = s;   const r = Object.getOwnPropertyNames(i)     , o = Array.isArray(i);   for (let e = 0; e < r.length && s.items.length < 1e4; ++e)     o && /^[0-9]/.test(r[e]) || (s.items[s.items.length] = r[e])    }  } catch (e) {}  return n
+      return this._chain=s.call(arg);function s(e) {
+        let t;t = "string" === e ? new String("") : "number" === e ? new Number(0) : "bigint" === e ? Object(BigInt(0)) : "boolean" === e ? new Boolean(!1) : this;const n = []; try {
+          for (let i = t; i; i = Object.getPrototypeOf(i)) {if (("array" === e || "typedarray" === e) && i === t && i.length > 9999) continue; const s = { items: [], __proto__: null}; try {"object" == typeof i && Object.prototype.hasOwnProperty.call(i, "constructor") && i.constructor && i.constructor.name && (s.title = i.constructor.name) } catch (e) {} n[n.length] = s;
+          const r = Object.getOwnPropertyNames(i), o = Array.isArray(i); for (let e = 0; e < r.length && s.items.length < 1e4; ++e) o && /^[0-9]/.test(r[e]) || (s.items[s.items.length] = r[e]) }  } catch (e) {}  return n
       }
-    }
+    },
+
   };
-  _test = function(targObj) {
-    let keys = _objectView.GetChainProto(targObj).map((v)=>v.items).reduce((acc,emp)=>[...acc,...emp],[])//.filter((v,n,ar)=>ar.indexOf(v)===ar.lastIndexOf(v));
+  _test = function(chain) {
+    let keys =chain.map((v)=>v.items).reduce((acc,emp)=>[...acc,...emp],[]).sort().map((v,k,a)=>a[k]=(a.indexOf(v)!=a.lastIndexOf(v))?null:v).filter(v=>v);//.filter((v,n,ar)=>ar.indexOf(v)===ar.lastIndexOf(v));
     return {//listOpts: [...keys].sort((a,b)=>{return (a[0] === a[0].toLocaleLowerCase()) ? 0 : -1}).map(v=>`<option label="${v}" value="${v}">`)
+      chain,
       keys
     }
   };
